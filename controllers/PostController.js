81bfbba434b0e1e295cdaeb41d368877da53e932
bleak <ffeeaarrofdeath@mail.ru>
2022-11-15 16:59:08 +0300

    half

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -39,8 +39,43 @@ export const getOne = async (req, res) => {
             message: "Статья не найдена",
           });
         }
- 
+
         res.json(doc);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            message: "Не удалить статью",
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json({
+          success: true,
+        });
       }
     );
   } catch (error) {
@@ -67,6 +102,34 @@ export const create = async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: "Не удалось создать статью",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось обновить статью",
     });
   }
 };
